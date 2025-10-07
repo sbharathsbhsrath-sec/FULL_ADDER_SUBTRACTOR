c274318c4fed4c1f6f02761b0aed9c7664806296
sbharathsbhsrath-sec <sbharathsbhsrath@gmail.com>
2025-10-07 14:29:02 +0530

    https://github.com/sbharathsbhsrath-sec/FULL_ADDER_SUBTRACTOR/upload/main

---
+++ fullsub3.v
@@ -0,0 +1,6 @@
+module fullsub3(a,b,bin,difference,borrow); 
+input a,b,bin; 
+output difference,borrow; 
+assign difference= ( (a ^ b)^bin); 
+assign borrow= ( ( ~a & b)| ( bin & (~(a ^ b )))); 
+endmodule 
